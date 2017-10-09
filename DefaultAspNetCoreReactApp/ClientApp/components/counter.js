cae29b793bfe337c15f4879377b6e30c2bea5fcc
Mihai Lazar <mihai.lazar@evozon.com>
2017-10-09 11:05:35 +0300

    Integrate a simple devexpress react grid

--- DefaultAspNetCoreReactApp/ClientApp/components/counter.js
+++ DefaultAspNetCoreReactApp/ClientApp/components/counter.js
@@ -1,25 +1,32 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
-// interface CounterState {
-//     currentCount: number;
-// }
 
-export class Counter extends React.Component/*<RouteComponentProps<{}>, CounterState>*/ {
+import DevGrid from './dev-grid';
+
+interface CounterState {
+    currentCount: number;
+}
+
+export class Counter extends React.Component<RouteComponentProps<{}>, CounterState> {
     constructor() {
         super();
         this.state = { currentCount: 0 };
     }
 
-    /*public*/ render() {
+    public render() {
         return <div>
-            <h1>Counter features some </h1>
+            <h1>Counter</h1>
 
             <p>This is a simple example of a React component.</p>
 
             <p>Current count: <strong>{ this.state.currentCount }</strong></p>
 
             <button onClick={ () => { this.incrementCounter() } }>Increment</button>
+
+            <div>
+                <DevGrid />
+            </div>
         </div>;
     }
 
---
+++ DefaultAspNetCoreReactApp/ClientApp/components/dev-grid.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {
+    Grid, TableView, TableHeaderRow    
+  } from '@devexpress/dx-react-grid-bootstrap3'/* or '@devexpress/dx-react-grid-material-ui' */;
+  
+const App = () => (
+    <Grid
+      rows={[
+        { id: 0, product: 'DevExtreme', owner: 'DevExpress' },
+        { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' },
+      ]}
+      columns={[
+        { name: 'id', title: 'ID' },
+        { name: 'product', title: 'Product' },
+        { name: 'owner', title: 'Owner' },
+      ]}>
+      <TableView />
+      <TableHeaderRow />
+    </Grid>
+  );
+
+  export default App;
